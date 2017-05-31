676e1bd54e56e73490c42e79c3f19a03d42d1894
sancas <sancas19@gmail.com>
2017-05-30 18:58:34 -0600

    Agregado diseño principal y se agrego Metro

--- ChatBot/ChatBot/Form1.Designer.cs
+++ ChatBot/ChatBot/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ChatBot
 {
-    partial class Form1
+    partial class frmPrincipal
     {
         /// <summary>
         /// Required designer variable.
@@ -29,11 +29,168 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.metroStyleManager = new MetroFramework.Components.MetroStyleManager(this.components);
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtMessage = new MetroFramework.Controls.MetroTextBox();
+            this.btnSend = new MetroFramework.Controls.MetroButton();
+            this.txtChat = new MetroFramework.Controls.MetroTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroStyleManager
+            // 
+            this.metroStyleManager.Owner = this;
+            this.metroStyleManager.Style = MetroFramework.MetroColorStyle.Red;
+            this.metroStyleManager.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtChat, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(20, 60);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(260, 220);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 75F));
+            this.tableLayoutPanel2.Controls.Add(this.txtMessage, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnSend, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 185);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(260, 35);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // txtMessage
+            // 
+            // 
+            // 
+            // 
+            this.txtMessage.CustomButton.Image = null;
+            this.txtMessage.CustomButton.Location = new System.Drawing.Point(147, 2);
+            this.txtMessage.CustomButton.Name = "";
+            this.txtMessage.CustomButton.Size = new System.Drawing.Size(23, 23);
+            this.txtMessage.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtMessage.CustomButton.TabIndex = 1;
+            this.txtMessage.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtMessage.CustomButton.UseSelectable = true;
+            this.txtMessage.CustomButton.Visible = false;
+            this.txtMessage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtMessage.FontSize = MetroFramework.MetroTextBoxSize.Medium;
+            this.txtMessage.Lines = new string[0];
+            this.txtMessage.Location = new System.Drawing.Point(3, 3);
+            this.txtMessage.MaxLength = 32767;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.PasswordChar = '\0';
+            this.txtMessage.PromptText = "Digite su mensaje...";
+            this.txtMessage.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtMessage.SelectedText = "";
+            this.txtMessage.SelectionLength = 0;
+            this.txtMessage.SelectionStart = 0;
+            this.txtMessage.ShortcutsEnabled = true;
+            this.txtMessage.Size = new System.Drawing.Size(179, 29);
+            this.txtMessage.TabIndex = 0;
+            this.txtMessage.UseSelectable = true;
+            this.txtMessage.UseStyleColors = true;
+            this.txtMessage.WaterMark = "Digite su mensaje...";
+            this.txtMessage.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtMessage.WaterMarkFont = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // btnSend
+            // 
+            this.btnSend.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnSend.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnSend.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.btnSend.Highlight = true;
+            this.btnSend.Location = new System.Drawing.Point(188, 3);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(69, 29);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Enviar";
+            this.btnSend.UseSelectable = true;
+            this.btnSend.UseStyleColors = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtChat
+            // 
+            // 
+            // 
+            // 
+            this.txtChat.CustomButton.Image = null;
+            this.txtChat.CustomButton.Location = new System.Drawing.Point(82, 2);
+            this.txtChat.CustomButton.Name = "";
+            this.txtChat.CustomButton.Size = new System.Drawing.Size(169, 169);
+            this.txtChat.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtChat.CustomButton.TabIndex = 1;
+            this.txtChat.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtChat.CustomButton.UseSelectable = true;
+            this.txtChat.CustomButton.Visible = false;
+            this.txtChat.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtChat.Enabled = false;
+            this.txtChat.FontSize = MetroFramework.MetroTextBoxSize.Medium;
+            this.txtChat.Lines = new string[0];
+            this.txtChat.Location = new System.Drawing.Point(3, 3);
+            this.txtChat.MaxLength = 32767;
+            this.txtChat.Multiline = true;
+            this.txtChat.Name = "txtChat";
+            this.txtChat.PasswordChar = '\0';
+            this.txtChat.ReadOnly = true;
+            this.txtChat.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtChat.SelectedText = "";
+            this.txtChat.SelectionLength = 0;
+            this.txtChat.SelectionStart = 0;
+            this.txtChat.ShortcutsEnabled = true;
+            this.txtChat.Size = new System.Drawing.Size(254, 179);
+            this.txtChat.TabIndex = 1;
+            this.txtChat.UseSelectable = true;
+            this.txtChat.UseStyleColors = true;
+            this.txtChat.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtChat.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(300, 300);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "frmPrincipal";
+            this.Style = MetroFramework.MetroColorStyle.Default;
+            this.Text = "Chat Bot";
+            this.Theme = MetroFramework.MetroThemeStyle.Default;
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        public MetroFramework.Components.MetroStyleManager metroStyleManager;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private MetroFramework.Controls.MetroTextBox txtMessage;
+        private MetroFramework.Controls.MetroButton btnSend;
+        private MetroFramework.Controls.MetroTextBox txtChat;
     }
 }
 
--- ChatBot/ChatBot/Form1.cs
+++ ChatBot/ChatBot/Form1.cs
@@ -7,14 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MetroFramework.Forms;
 
 namespace ChatBot
 {
-    public partial class Form1 : Form
+    public partial class frmPrincipal : MetroForm
     {
-        public Form1()
+        public frmPrincipal()
         {
             InitializeComponent();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            this.StyleManager = metroStyleManager;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            txtChat.Text += "Tu: " + txtMessage.Text + "\r\n";
+            txtMessage.Clear();
         }
     }
 }
